22b6f7adefa5a613306656555445e4d75230d116
Sorochan Ilya <fullof.acedia@gmail.com>
2022-12-26 00:25:13 +0300

    0 prog - random implemented

--- build.zig
+++ build.zig
@@ -3,6 +3,10 @@ const std = @import("std");
 pub fn build(b: *std.build.Builder) void {
     const debug = b.addExecutable("debug", "src/main.zig");
     debug.setOutputDir("build");
+    debug.addPackage(.{
+        .name = "read",
+        .path = .{ .path = "src/read.zig" },
+    });
     
     const debug_step = b.step("debug", "Compile debug build");
     debug_step.dependOn(&debug.step);
@@ -13,6 +17,10 @@ pub fn build(b: *std.build.Builder) void {
     const linux = b.addExecutable("linux", "src/main.zig");
     linux.setBuildMode(.ReleaseFast);
     linux.setOutputDir("build");
+    linux.addPackage(.{
+        .name = "read",
+        .path = .{ .path = "src/read.zig" },
+    });
 
     const linux_step = b.step("linux", "Compile linux release build");
     linux_step.dependOn(&linux.step);
@@ -20,9 +28,16 @@ pub fn build(b: *std.build.Builder) void {
     const windows = b.addExecutable("windows.exe", "src/main.zig");
     windows.setBuildMode(.ReleaseFast);
     windows.setOutputDir("build");
+    windows.addPackage(.{
+        .name = "read",
+        .path = .{ .path = "src/read.zig" },
+    });
 
     const windows_step = b.step("windows", "Compile windows release build");
     windows_step.dependOn(&windows.step);
 
-    // const test_step = b.step("test", "Run unit tests");
+    const test_random = b.addTest("src/prog/random.zig");
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&test_random.step);
 }
---
+++ src/prog/random.zig
@@ -0,0 +1,49 @@
+const std = @import("std");
+const readInt = @import("read").readInt;
+const log = std.log.debug;
+
+/// LCG constants
+const m = 2147483648;
+const a = 1103515245;
+const c = 12345;
+
+/// Linear congruential generator (LGC)
+inline fn lgc(seed: *i64, data: []i64) void {
+    for (data) |*item| {
+        item.* = @mod(a * seed.* + c, m);
+        seed.* = item.*;
+    }
+}
+
+/// Program interface for LCG
+pub fn rand(allocator: std.mem.Allocator, out: bool) !void {
+    log("Enter seed: ", .{});
+    var seed = try readInt(i64);
+
+    log("Enter n: ", .{});
+    const n = try readInt(usize);
+
+    var data = try allocator.alloc(i64, n);
+
+    var timer = try std.time.Timer.start();
+    lgc(&seed, data);
+    const estimated = timer.read() / std.time.ns_per_ms;
+
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("time: {d} ms\n", .{estimated});
+    if (out) try stdout.print("{any}\n", .{data});
+}
+
+test "random" {
+    const allocator = std.testing.allocator;
+    var data = try allocator.alloc(i64, 10);
+    defer allocator.free(data);
+
+    var seed: i64 = 7;
+    lgc(&seed, data);
+
+    const precalc = [_]i64{ 1282168116, 642666333, 712265938, 1486001571, 2131988640, 220562521, 2099423262, 2083449087, 523310796, 715197717 };
+    for (precalc) |val, i| {
+        try std.testing.expectEqual(val, data[i]);
+    }
+}
